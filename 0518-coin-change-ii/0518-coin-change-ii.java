2977f00ebb373324023cb2b8aca4bfb9d83ef2bf
Swastika <riddhigupta656@gmail.com>
2026-08-06 19:10:18 +0530

    Time: 10 ms (66.12%), Space: 43.2 MB (66.96%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        Arrays.sort(coins);
+        int dp[]= new int[amount+1];
+        dp[0]=1;
+        for(int i=0; i<coins.length; i++){
+            for(int j=1; j<= amount; j++){
+                if(j>= coins[i]){
+                    dp[j]= dp[j]+dp[j- coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
